45a4ed583824f1665ae42605eb349db5c3fb13dc
Mahmoud Awadeen <mahmoud.awadeen@gmail.com>
2023-06-08 17:15:23 +0200

    [fix] add relations pinia-orm complains about

--- src/Audio.js
+++ src/Audio.js
@@ -1,4 +1,5 @@
 import { Model } from 'pinia-orm'
+import Todo from "@/Todo";
 
 export default class Audio extends Model {
     static entity = 'audios'
@@ -7,6 +8,9 @@ export default class Audio extends Model {
         return {
             id: this.uid(),
             name: this.string(''),
+            // WHY ?!
+            todos: this.hasMany(Todo, 'audioId'),
+            media: this.morphMany(Todo, 'mediaId', 'mediaType'),
         }
     }
     static piniaOptions = {
